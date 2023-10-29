c50192c7b7ec551309dfe3ef574d605bb6753510
Rodrigo Calle <54957230+RodriCalle@users.noreply.github.com>
2023-10-28 23:05:54 -0500

    cambios para api vertex ai

--- app.py
+++ app.py
@@ -101,19 +101,81 @@ def generate_outfits(request_data):
     print(outfits_array)
 
     # generar outfits images
-    generator = Craiyon()
+    # generator = Craiyon()
 
-    for outfit in outfits_array:
-        prompt_to_craiyon = generate_promt_to_craiyon(request_data, outfit)
-        outfit['name'] = prompt_to_craiyon
-        try:
-            result = generator.generate(prompt= prompt_to_craiyon, negative_prompt="accessories", model_type="photo")
-            outfit['images'] = result.images
+    # for outfit in outfits_array:
+    #     prompt_to_craiyon = generate_promt_to_craiyon(request_data, outfit)
+    #     outfit['name'] = prompt_to_craiyon
+    #     try:
+    #         result = generator.generate(prompt= prompt_to_craiyon, negative_prompt="accessories", model_type="photo")
+    #         outfit['images'] = result.images
             
-        except Exception as e:
-            print("Error generando imagen: ", e)
+    #     except Exception as e:
+    #         print("Error generando imagen: ", e)
+
+    # generar outfits images
+    outfits_array = generate_images_from_prompt(request_data, outfits_array)
 
     return outfits_array
+
+def generate_images_from_prompt(request_data, outfits_array):
+    PROJECT_ID = "scenic-aileron-389223"
+    FILE_NAME = "scenic-aileron-389223-00fedf4349a3.json"
+    API_URL = "https://us-central1-aiplatform.googleapis.com/"
+
+    url = f"{API_URL}v1/projects/{PROJECT_ID}/locations/us-central1/publishers/google/models/imagegeneration:predict"
+
+    with open(FILE_NAME, "r") as file:
+        data = json.load(file)
+
+    iat = time.time()
+    exp = iat + 3600
+    payload = {'iss': data['client_email'],
+               'sub': data['client_email'],
+               'aud': API_URL,
+               'iat': iat,
+               'exp': exp}
+    additional_headers = {'kid': data['private_key_id']}
+    signed_jwt = jwt.encode(payload, data['private_key'], headers=additional_headers, algorithm='RS256')
+
+    headers = {
+        "Authorization": f"Bearer {signed_jwt}",
+        "Content-Type": "application/json"
+    }
+
+    for outfit in outfits_array:
+        prompt = generate_promt_to_craiyon(request_data, outfit)
+
+        data = {
+          "instances": [
+            {
+              "prompt": "Photo centered of a " + prompt
+            }
+          ],
+          "parameters": {
+            "sampleCount": 1
+          }
+        }
+
+        try:
+            response = requests.post(url, headers=headers, json=data)
+
+            if response.status_code == 200:
+                response_data = response.json()
+
+                for prediction in response_data['predictions']:
+                    imageb64 = prediction['bytesBase64Encoded']
+                    mimeType = prediction['mimeType']
+                    outfit['image'] = imageb64
+                    outfit['name'] = prompt
+            else:
+                outfit['images'] = ""
+                outfit['name'] = ""
+                print(f"Error {response.status_code}: {response.text}")
+        except Exception e:
+            print("Ocurrio una excepcion")
+    
+    return outfits_array 
 
 @app.route('/closet', methods=['POST'])
 def get_cloths():
