00aefcaf8923c7088f7547a57bbab3861d231264
Rodrigo <rodrigocallegaldos@gmail.com>
2023-11-14 20:53:53 -0500

    cambios

--- chat_openai.py
+++ chat_openai.py
@@ -1,18 +1,18 @@
 
-import openai
+from openai import OpenAI
 import json
 
-openai.organization = "org-yJpmbqbka0cb7xqz5uPDYmmq"
-openai.api_key = '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'
-
 quantity = "two"
+OPENAI_API_KEY="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"
 
-completion = openai.ChatCompletion.create(
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+completion = client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         response_format={ "type": "json_object" },
         messages=[
           {"role": "system", 
-           "content": f'''You are a fashion expert specializing in clothing design. You will receive descriptions of clothing items, including the type, color, style, and gender. Additionally, the description will include the temperature of the environment. Your task is to provide {quantity} outfits in JSON format. Each outfit should be represented as a JSON object within an array, with attributes for "top," "bottom," and "shoes." Ensure that each attribute is a string describing the corresponding clothing item.'''},
+           "content": f'''You are a fashion expert specialized in clothing design. You will receive a description of an item of clothing, including type, color, style, and gender. Additionally, the description includes the ambient temperature. Your task is to provide {quantity} sets of clothing that include the item and fit the main description. Each set should be represented as a JSON object, with attributes for "top", "bottom", and "shoes". Make sure each attribute is a string that describes the corresponding item of clothing in the format of: style color item of clothing. The response must be a JSON object with an outfits element that is an array of the generated objects.'''},
           {"role": "user", 
            "content": "Casual Blue T-Shirt for summer in 25 degrees Celsius"}
         ]
